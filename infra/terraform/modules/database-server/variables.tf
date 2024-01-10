7ebcb1b10c6ac50523148803163ac52ab7cdfc18
yollollay <yollollay@gmail.com>
2024-01-10 10:18:50 +0600

    [add] terraform.tf and variables for db-server and frontend-server

---
+++ infra/terraform/modules/database-server/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "size" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "public_key"{
+  type = string
+}
+
+variable "private_ip_address" {
+  type = string
+}
